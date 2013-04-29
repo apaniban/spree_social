28918da8b6ef091d6f7658949c15ab6d2793dde7
Aljon Aniban <aljon.aniban@bazinga.com.ph>
2013-04-29 17:49:57 +0800

    pulled username, firstname, lastname, and gender

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -6,6 +6,10 @@ Spree.user_class.class_eval do
   def apply_omniauth(omniauth)
     if omniauth['provider'] == "facebook"
       self.email = omniauth['info']['email'] if email.blank?
+      self.username = omniauth['extra']['raw_info']['username'] if username.blank?
+      self.firstname = omniauth['info']['first_name'] if firstname.blank?
+      self.lastname = omniauth['info']['last_name'] if lastname.blank?
+      self.gender = omniauth['extra']['raw_info']['gender'] if gender.blank?
     end
     user_authentications.build(:provider => omniauth['provider'], :uid => omniauth['uid'])
   end
